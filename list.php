27cf0d1d92bcf14e657fa91d8736d137f9c4ba27
HafshahNada <42725368+HafshahNada@users.noreply.github.com>
2018-10-05 19:30:21 +0700

    Update list.php

--- list.php
+++ list.php
@@ -1,37 +1,46 @@
-<!DOCTYPE html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <table>
-           
-        <form action="submit.php" method="post">
-             <tr>
-                <td>Nama</td>
-                <td>:</td>
-                <td><input type="text" name="nama" id=""></td><br>
-            
-             </tr>
+<?php
+require_once 'db.php';
+?>
+<table border=1>
+    <thead>
+        <th>Nama</th>
+        <th>Nim</th>
+        <th>Tanggal Lahir</th>
+    </thead>
+    <tbody>
+<?php
+$query    = "SELECT * FROM Siswa";
+$result = mysqli_query($conn, $query);
 
-             <tr>
-                 <td>Nim</td>
-                 <td>:</td>
-                <td><input type="text" name="nim" id=""></td><br>
-               
-            </tr>
-           
-            <tr>
-                 <td>Tanggal Lahir<td>
-                 <td>:</td>
-                 <td><input type="date" name="tgl_lahir" id=""></td>
-              
-            </tr>
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+?>
 
-            <tr>
-                 <td> <input type="submit" value="Kirim"></td>
-            </tr>
+    <tr>
+        <td><?php echo $row['nama']; ?></td>
+        <td><?php echo $row['nim']; ?></td>
+        <td><?php echo $row['tgl_lahir']; ?></td>
+        <td>
+            <a href="Edit.php?id=<?php echo $row['id']; ?>">Edit</a> 
+            <a href="Delete.php?id=<?php echo $row['id']; ?>" on click=return confirm('Apakah Anda Yakin?'>Delete</a>
+        </td>
+    </tr>
 
-     </form>
-    </table>
-    </body>
+<?php
+        }
+    }else{
+        echo "No result";
+    }
+?>
+</tbody>
+</table>
+<br>
+    <a href="form.php">Tambah Data</a>
+
+</body>
 </html>
+
+        
+        
+        
