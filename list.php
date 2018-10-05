96813d942c957d80f52f1f86b2bbcbbe3fa59fbb
HafshahNada <42725368+HafshahNada@users.noreply.github.com>
2018-10-05 19:29:24 +0700

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,37 @@
-<?php
-require_once("db.php");git 
-?>
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
+<!DOCTYPE html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <table>
+           
+        <form action="submit.php" method="post">
+             <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text" name="nama" id=""></td><br>
+            
+             </tr>
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
-</table>
+             <tr>
+                 <td>Nim</td>
+                 <td>:</td>
+                <td><input type="text" name="nim" id=""></td><br>
+               
+            </tr>
+           
+            <tr>
+                 <td>Tanggal Lahir<td>
+                 <td>:</td>
+                 <td><input type="date" name="tgl_lahir" id=""></td>
+              
+            </tr>
+
+            <tr>
+                 <td> <input type="submit" value="Kirim"></td>
+            </tr>
+
+     </form>
+    </table>
+    </body>
+</html>
